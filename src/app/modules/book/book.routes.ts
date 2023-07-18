fd2630918b828b3c23cb696465369b2ef7ae897b
progmamun <mamun333089@gmail.com>
2023-07-18 22:04:08 +0600

    refactor

--- src/app/modules/book/book.routes.ts
+++ src/app/modules/book/book.routes.ts
@@ -1,7 +1,5 @@
 import express from 'express';
-import { ENUM_USER_ROLE } from '../../../enums/user';
 import { BookController } from './book.controller';
-import auth from '../../middleware/auth';
 
 const router = express.Router();
 
@@ -13,16 +11,8 @@ router.get('/:slug', BookController.getSingleBook);
 
 router.get('/', BookController.getAllBooks);
 
-router.patch(
-  '/:slug',
-  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.AUTHOR),
-  BookController.updateBook
-);
+router.patch('/:slug', BookController.updateBook);
 
-router.delete(
-  '/:slug',
-  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.AUTHOR),
-  BookController.deleteBook
-);
+router.delete('/:slug', BookController.deleteBook);
 
 export const BookRoutes = router;
